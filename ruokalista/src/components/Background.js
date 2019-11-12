64b6b45f6084f08e027bd19cfe131dc42613c053
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-11-12 20:01:18 +0200

    update

--- ruokalista/src/components/Background.js
+++ ruokalista/src/components/Background.js
@@ -2,10 +2,22 @@ import React, {Component} from 'react';
 import '../css/App.css';
 
 class Background extends Component {
-  render() {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      videoURL: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+    }
+  }
+
+  render () {
     return (
-        <div className='bg'/>
-    );
+        <video id="myVideo" loop autoPlay controls muted>
+          <source src={this.state.videoURL} type="video/mp4" />
+          <source src={this.state.videoURL} type="video/ogg" />
+          Your browser does not support the video tag.
+        </video>
+    )
   }
 }
 
